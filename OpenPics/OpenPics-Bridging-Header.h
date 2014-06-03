a1ce3344cf179943bf7e2973b4fd0099c4f1f3b8
PJ Gray <pj@evertrue.com>
2014-06-03 12:13:06 -0700

    Added a derived AFHTTPSessionManager written in Swift

--- OpenPics/OpenPics-Bridging-Header.h
+++ OpenPics/OpenPics-Bridging-Header.h
@@ -14,4 +14,5 @@
 #import "TMCache.h"
 #import "AFNetworkActivityIndicatorManager.h"
 #import "AFNetworkActivityLogger.h"
+#import "AFHTTPSessionManager.h"
 
---
+++ OpenPics/Providers/LOC/LOCSessionManager.swift
@@ -0,0 +1,26 @@
+//
+//  LOCSessionManager.swift
+//  OpenPics
+//
+//  Created by PJ Gray on 6/3/14.
+//  Copyright (c) 2014 Say Goodnight Software. All rights reserved.
+//
+
+import UIKit
+
+var kLOCBaseURLString = "http://www.loc.gov/pictures/"
+
+class LOCSessionManager: AFHTTPSessionManager {
+   
+    struct Shared {
+        static var Instance: LOCSessionManager = LOCSessionManager(baseURL: NSURL(string: kLOCBaseURLString))
+    }
+    
+    init(baseURL url: NSURL!) {
+        super.init(baseURL: url)
+        let responseSerializer = AFJSONResponseSerializer()
+        responseSerializer.acceptableContentTypes = responseSerializer.acceptableContentTypes .setByAddingObject("text/plain")
+        self.responseSerializer = responseSerializer
+    }
+
+}
